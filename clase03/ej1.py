c21b5cdc83cdf7f2b6243aa0f9d13195791c5f2c
malopez@vates.com <malopez@vates.com>
2017-09-26 16:59:30 -0300

    ejercicios clase 3

--- clase03/ej1.py
+++ clase03/ej1.py
@@ -1,8 +1,9 @@
 """
 Crear una funcion que obtenga informacion del sistema usando subprocess.
  - Obtener las salidas de los comandos "cat /proc/cpuinfo", "free -h" y "cat /proc/loadavg" y guardarlos en una variable
- - Imprimir la informacion obtenida en el siguiente formato:
  - Guardad datos en un log
+ - Imprimir la informacion obtenida en el siguiente formato:
+
 CPU: <model_name>
 Uso de CPU: <Load_average_1>
 Mem RAM total: <mem_total>
--- clase03/ej3.py
+++ clase03/ej3.py
@@ -1,9 +1,10 @@
 """
-Utilizando subprocess
- - Crear una funcion, donde en un thread se cosulte los nuevos mensajes en el chat cada 10 seg y guardarlos en una lista
- - Cuando exista un nuevo mensaje imprimir "Mensajes sin leer: (LEN LISTA)"
- - En el thead principal crear un bucle esperando 3 opciones: 1- mostrar mensajes 2- enviar mensaje 3- salir
-   mostrar mensajes: ejecuta la funcion get_chat()
-   enviar mensaje_ ejecuta la funcion  send_mensaje()
-   salir: Termina el programa
+Utilizando Threads
+ - Crear una funcion, donde en un thread se cosulte los nuevos mensajes en el chat cada 10 seg, si existen
+   nuevos mensajes imprimirlo en pantalla"
+
+ - Al ininciar preguntar nombre de usuario y guardarlo en una variable
+
+ - En el thread principal crear un bucle esperando ingresar un nuevo comentario al chat
+
 """
